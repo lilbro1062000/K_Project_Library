8c17d197b8d5ba96613d819103bcb3bceb56bbac
Abdoulaye Camara <lilbro1062000@gmail.com>
2011-11-04 00:58:25 -0400

    Updates

--- Wii tracking6dof/Form1.cs
+++ Wii tracking6dof/Form1.cs
@@ -6,14 +6,23 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using WiimoteLib;
+using System.Threading;
 
 namespace Wii_tracking6dof
 {
     public partial class Form1 : Form
     {
+        bool connected;
+        Wii_stuff w = new Wii_stuff();
         public Form1()
         {
             InitializeComponent();
+            Thread wii_thread = new Thread(new ThreadStart(w.wii_stuff));
+
+            
+            
         }
+     
     }
 }
---
+++ Wii tracking6dof/Wii_stuff.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WiimoteLib;
+
+namespace Wii_tracking6dof
+{
+    class Wii_stuff
+    {
+        bool connected;
+        public void wii_stuff()
+        {
+            // create a new instance of the Wiimote
+            Wiimote wm = new Wiimote();
+
+            // setup the event to handle state changes
+            wm.WiimoteChanged += new EventHandler<WiimoteChangedEventArgs>(wm_WiimoteChanged);
+
+            // setup the event to handle insertion/removal of extensions
+            wm.WiimoteExtensionChanged += new EventHandler<WiimoteExtensionChangedEventArgs>(wm_WiimoteExtensionChanged);
+
+            // connect to the Wiimote
+            connected = false;
+            check_for_wii_remotes(wm);
+
+            // set the report type to return the IR sensor and accelerometer data (buttons always come back)
+
+            wm.SetReportType(InputReport.IRAccel, true);
+        }
+
+        public string  check_for_wii_remotes(Wiimote wm)
+        {
+           
+                try
+                {
+                    wm.Connect();
+                    connected = true;
+                    return "Connected";
+
+                }
+                catch (WiimoteLib.WiimoteNotFoundException ex)
+                {
+                    return ex.Message;
+
+                }
+
+           
+        }
+
+        void wm_WiimoteExtensionChanged(object sender, WiimoteExtensionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        void wm_WiimoteChanged(object sender, WiimoteChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
